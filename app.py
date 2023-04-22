eebc00ccf0b06d3a97ec493b6ff647f2de8adf24
marcelosousa-utexas <112403984+marcelosousa-utexas@users.noreply.github.com>
2023-04-22 02:13:10 +0000

    load from db

--- app.py
+++ app.py
@@ -1,37 +1,33 @@
 from flask import Flask, render_template
 
+from database import engine
+from sqlalchemy import text
+
 app = Flask(__name__)
 
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote'
-  },
-  {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$150,000'
-  }
-]
+
+
+def load_jobs_from_db():
+  results_to_dict = []
+  
+  with engine.connect() as connection:
+    result = connection.execute(text("select * from jobs"))
+    #results_to_dict = []
+    for row in result.all():
+      results_to_dict.append(row._asdict())
+    #  results_to_dict.append(dict(row))
+  return results_to_dict
+  #print(results_to_dict)
+    #print(result.all())
+
+  
+#print(sqlalchemy.__version__)  
 
 @app.route("/")
 def hello_word():
-  return render_template("home.html", jobs=JOBS)
+  results_to_dict = load_jobs_from_db()
+  return render_template("home.html", jobs=results_to_dict)
   #return "<p> Hello, world <p>"
 
 #if __name__ == "__name__":  
--- database.py
+++ database.py
@@ -1,24 +1,27 @@
 #import sqlalchemy
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://0b9zcnwnos296ynimcb1:pscale_pw_WGDIbhpuHN5CS3uGudG7p4yKQZd9k5LEsmR1X8cbGpN@aws.connect.psdb.cloud/my-first-database?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://hx90bmc7gyqxshlhb5wq:pscale_pw_FNBu1z2nXB2bhyTCMt6SeUUy1Z0LNJmpxRTh7c5gSrS@aws.connect.psdb.cloud/my-first-database?charset=utf8mb4"
 
 connection_ssl_arg = {
   "ssl": {
-    "ca": "/etc/ssl/cert.pem"
+    "ssl_ca": "/etc/ssl/cert.pem"
   }
 } 
 
 engine = create_engine(db_connection_string, connect_args = connection_ssl_arg)
 
+results_to_dict = []
 
 with engine.connect() as connection:
-    result = connection.execute(text("select * from jobs"))
-#    for row in result:
-#        print("title:", #row["location"])
+  result = connection.execute(text("select * from jobs"))
+  #results_to_dict = []
+  for row in result.all():
+    results_to_dict.append(row._asdict())
+  #  results_to_dict.append(dict(row))
 
-print(result.all())
-
+print(results_to_dict)
+  #print(result.all())
 
   
 #print(sqlalchemy.__version__)
