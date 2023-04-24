d7d61b251319f4c8ac9bc513c67ab09cb83f4c52
marcelosousa-utexas <112403984+marcelosousa-utexas@users.noreply.github.com>
2023-04-24 06:29:45 +0000

    trying to fix post

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import nltk
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db
 from classificator import build_parameter
 
@@ -19,7 +19,7 @@ print(__name__)
 @app.route("/")
 def hello_word():
   #n_classifications, n_parameters
-  par = build_parameter(2,6)
+  par = build_parameter(1,6)
   classification_list, parameter_matrix = par.create_parameter_matrix()
   return render_template("home.html", classifications=classification_list, class_parameters=parameter_matrix)
   #return "<p> Hello, world <p>"
@@ -28,6 +28,21 @@ def hello_word():
 def list_jobs():
   results_to_dict = load_jobs_from_db()
   return jsonify(results_to_dict)
+
+# @app.route("/model_result", methods=['POST'])
+@app.route("/model_result", methods=['GET', 'POST'])
+def model_result():
+    if request.method == 'POST':
+        #data = request.method 
+        #print(data)
+        return render_template('model_result.html', class_1 = request.form['class_1'])
+      
+    elif request.method == 'GET':
+        print('A GET request was made')
+        #return render_template('model_result.html', model_result=request.form['model_result'])
+
+    else:
+        return 'Not a valid request method for this route'
 
 #if __name__ == "app":
 if __name__ == "__main__":  
